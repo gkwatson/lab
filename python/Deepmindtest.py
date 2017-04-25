4fc008950095299f55fec8410fdc8ccc7510c330
Gavin <gkwatson@email.wm.edu>
2017-04-24 21:41:41 -0400

    Single layer Q-Learning approximation seems to be working

--- python/Deepmindtest.py
+++ python/Deepmindtest.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 # Construct and start the environment
 env = deepmind_lab.Lab('seekavoid_arena_01', ['RGB_INTERLACED'], config={
 	'fps': str(60),
-	'width': str(720),
-	'height': str(480)})
+	'width': str(360),
+	'height': str(240)})
 env.reset()
 
 # Create action dictionary
@@ -25,6 +25,21 @@ ACTIONS = {
   'crouch': np.array((0, 0, 0, 0, 0, 0, 1), dtype=np.intc)
 }
 
+action_list = [
+  np.array((-20, 0, 0, 0, 0, 0, 0), dtype=np.intc),
+  np.array((20, 0, 0, 0, 0, 0, 0), dtype=np.intc),
+  np.array((0, 10, 0, 0, 0, 0, 0), dtype=np.intc),
+  np.array((0, -10, 0, 0, 0, 0, 0), dtype=np.intc),
+  np.array((0, 0, -1, 0, 0, 0, 0), dtype=np.intc),
+  np.array((0, 0, 1, 0, 0, 0, 0), dtype=np.intc),
+  np.array((0, 0, 0, 1, 0, 0, 0), dtype=np.intc),
+  np.array((0, 0, 0, -1, 0, 0, 0), dtype=np.intc),
+  np.array((0, 0, 0, 0, 1, 0, 0), dtype=np.intc),
+  np.array((0, 0, 0, 0, 0, 1, 0), dtype=np.intc),
+  np.array((0, 0, 0, 0, 0, 0, 1), dtype=np.intc)
+]
+
+
 #Example
 old_obs = env.observations()
 reward = env.step(ACTIONS['forward'], num_steps = 4)
@@ -39,13 +54,16 @@ print type(rgb_i)
 
 #Tensorflow stuff
 
-array4tf = tf.constant(rgb_i)
-list4tf = []
+def turnRGBIIntoNPArray(rgbi):
+    list4tf = []
+    for width in rgbi:
+        for colors in width:
+            for color in colors:
+                #And normalize
+                list4tf.append(float(color)/rgbi.size)
+    return np.array([list4tf])
 
-for width in rgb_i:
-  for colors in width:
-    for color in colors:
-      list4tf.append(color)
+arrayOfState = turnRGBIIntoNPArray(rgb_i)
 
 #lstm = tf.contrib.rnn.BasicLSTMCell(128)
 ## Initial state of the LSTM memory.
@@ -60,13 +78,13 @@ for width in rgb_i:
 
 
 #These lines establish the feed-forward part of the network used to choose actions
-inputs1 = tf.placeholder(shape=[1,len(list4tf)],dtype=tf.float32)
-W = tf.Variable(tf.random_uniform([len(list4tf),7],0,0.01))
+inputs1 = tf.placeholder(shape=[1,arrayOfState.size],dtype=tf.float32)
+W = tf.Variable(tf.random_uniform([arrayOfState.size,8],0,0.01))
 Qout = tf.matmul(inputs1,W)
 predict = tf.argmax(Qout,1)
 
 #Below we obtain the loss by taking the sum of squares difference between the target and prediction Q values.
-nextQ = tf.placeholder(shape=[1,7],dtype=tf.float32)
+nextQ = tf.placeholder(shape=[1,8],dtype=tf.float32)
 loss = tf.reduce_sum(tf.square(nextQ - Qout))
 trainer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
 updateModel = trainer.minimize(loss)
@@ -87,39 +105,46 @@ with tf.Session() as sess:
         #Reset environment and get first new observation
         env.reset()
 	obs = env.observations()
-        list4tf = []
-        for width in obs['RGB_INTERLACED']:
-          for colors in width:
-            for color in colors:
-              list4tf.append(color)
-        s = np.array([list4tf])
+        s = turnRGBIIntoNPArray(obs['RGB_INTERLACED'])
  
         rAll = 0
         d = False
         j = 0
         #The Q-Network
-        while j < 99:
+        while j < 100:
+            print "Step: " + str(j)
             j+=1
-            #Choose an action by greedily (with e chance of random action) from the Q-network
+            #Choose an action (with e chance of random action) from the Q-network
             a,allQ = sess.run([predict,Qout],feed_dict={inputs1:s})
-            #if np.random.rand(1) < e:
-            #    a[0] = env.action_space.sample()
+
+            #Randomly sample from actions based on their Q values
+            selector = np.random.random() * np.sum(allQ[0])
+            run_sum = 0.0
+            a[0] = 7
+            for i in range(0, 8):
+                run_sum += allQ[0, i]
+                if selector < run_sum:
+                    a[0] = i
+                    break
+
+            #Possibility of random action
+            if np.random.rand(1) < e:
+                a[0] = np.random.randint(0, 8)
+
             #Get new state and reward from environment
             print "a = " + str(a)
             print "allQ = " + str(allQ)
-            r = env.step(allQ[0].astype(np.intc), num_steps = 1)
+            r = env.step(action_list[a[0]], num_steps = 10)
             obs = env.observations()
             d = env.is_running()
-            list4tf = []
-            for width in obs['RGB_INTERLACED']:
-              for colors in width:
-                for color in colors:
-                  list4tf.append(color)
-            s1 = np.array([list4tf])
+            s1 = turnRGBIIntoNPArray(obs['RGB_INTERLACED'])
+
             #Obtain the Q' values by feeding the new state through our network
             Q1 = sess.run(Qout,feed_dict={inputs1:s1})
             #Obtain maxQ' and set our target value for chosen action.
             maxQ1 = np.max(Q1)
+            print "Q1 = " + str(Q1)
+            print "maxQ1 = " + str(maxQ1)
             targetQ = allQ
             targetQ[0,a[0]] = r + y*maxQ1
             #Train our network using target and predicted Q values
